ae0928c3f3def87bcf9fbd5bb9462a7a2e724347
Оберман Игорь <oberman.ie@gmail.com>
2021-03-29 17:12:49 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include"functions_compiler.h"
+#include"functions_processor.h"
+
+int main()
+{
+    Compiler comp;
+    size_t number_of_blocks = comp.Compile("factorial.txt", "output.o");
+
+    Processor proc;
+    proc.Run("output.o", number_of_blocks);
+    return 0;
+}
